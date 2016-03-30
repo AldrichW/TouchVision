9be9f62ec35bd0bbe4e4595faf36877883336a99
Aldrich Wingsiong <aldrich.wingsiong@gmail.com>
2016-03-30 10:32:02 -0400

    UI work done! READY FOR DEMO

--- app/src/main/java/com/teamsight/touchvision/vWandConnectActivity.java
+++ app/src/main/java/com/teamsight/touchvision/vWandConnectActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.os.Handler;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 
 public class vWandConnectActivity extends Activity {
@@ -35,11 +40,10 @@ public class vWandConnectActivity extends Activity {
         super.onStart();
         assert(mT2SService != null && mVWandService != null);
 
-        mT2SService.speakText("V-Wand is Connecting.", TextToSpeechService.FLUSH_IF_BUSY);
-
         Thread thread = new Thread() {
             @Override
             public void run() {
+                mT2SService.speakText("Connecting to V-Wand. Please make sure V-Wand is powered on", TextToSpeechService.FLUSH_IF_BUSY);
                 while(!mVWandService.isVWandConnected()){
                     mVWandService.connectToVWand();
                 }
@@ -49,7 +53,17 @@ public class vWandConnectActivity extends Activity {
                 Runnable myRunnable = new Runnable() {
                     @Override
                     public void run() {
-                        startMainInterfaceActivity();
+                        try{
+                            showSuccessCheckmark();
+                            mT2SService.speakText("V-Wand Connected.", TextToSpeechService.FLUSH_IF_BUSY);
+                            sleep(3000); // sleep for 3 seconds
+                            startMainInterfaceActivity();
+                        }catch (InterruptedException e){
+                            System.out.println(e.getMessage());
+                            e.printStackTrace();
+                            startMainInterfaceActivity();
+                        }
+
                     }
                 };
                 mainHandler.post(myRunnable);
@@ -57,12 +71,40 @@ public class vWandConnectActivity extends Activity {
         };
 
         thread.start();
+
+        startBluetoothAnimation();  //When connecting to vWand show in progress by fading bluetooth image
     }
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
         mVWandService.disconnectVWand();
+    }
+
+    private void startBluetoothAnimation() {
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator()); //add this.
+        fadeIn.setDuration(1000);
+        fadeIn.setRepeatCount(Animation.INFINITE);
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
+        fadeOut.setStartOffset(1000);
+        fadeOut.setDuration(1000);
+        fadeOut.setRepeatCount(Animation.INFINITE);
+
+        AnimationSet animation = new AnimationSet(false); //change to false
+        animation.addAnimation(fadeIn);
+        animation.addAnimation(fadeOut);
+        animation.setRepeatMode(Animation.REVERSE);
+        animation.setRepeatCount(Animation.INFINITE);
+        bluetoothImage.setAnimation(animation);
+    }
+
+    private void showSuccessCheckmark(){
+        bluetoothImage.setMaxWidth(150);
+        bluetoothImage.setMaxHeight(150);
+        bluetoothImage.setImageResource(R.mipmap.success_small);
     }
 
 
